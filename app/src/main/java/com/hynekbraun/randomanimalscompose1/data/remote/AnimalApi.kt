88b6d598e10b00a9ecafeedb8c4aef05dec0283e
resonus <braun.hynek@gmail.com>
2022-05-04 04:47:02 +0200

    Adding api interface with retrofit and models

---
+++ app/src/main/java/com/hynekbraun/randomanimalscompose1/data/remote/AnimalApi.kt
@@ -0,0 +1,15 @@
+package com.hynekbraun.randomanimalscompose1.data.remote
+
+import com.hynekbraun.randomanimalscompose1.data.remote.dto.AnimalFactDto
+import retrofit2.http.GET
+import java.util.concurrent.Flow
+
+interface AnimalApi {
+
+    @GET("animals/rand/10")
+    suspend fun getAnimalList(): List<AnimalFactDto>
+
+    companion object {
+        val BASE_URL = "https://zoo-animal-api.herokuapp.com"
+    }
+}
---
+++ app/src/main/java/com/hynekbraun/randomanimalscompose1/domain/model/AnimalFact.kt
@@ -0,0 +1,18 @@
+package com.hynekbraun.randomanimalscompose1.domain.model
+
+data class AnimalFact(
+    val active_time: String,
+    val animal_type: String,
+    val diet: String,
+    val geo_range: String,
+    val habitat: String,
+    val id: Int,
+    val image_link: String,
+    val latin_name: String,
+    val length_max: String,
+    val length_min: String,
+    val lifespan: String,
+    val name: String,
+    val weight_max: String,
+    val weight_min: String
+)
