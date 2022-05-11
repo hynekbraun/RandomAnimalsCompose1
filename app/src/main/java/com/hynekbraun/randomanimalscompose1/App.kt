90f924ea596732a47c8d4eaacba881b73ae3fe94
resonus <braun.hynek@gmail.com>
2022-05-11 16:18:10 +0200

    Gradle update.

--- app/src/main/java/com/hynekbraun/randomanimalscompose1/App.kt
+++ app/src/main/java/com/hynekbraun/randomanimalscompose1/App.kt
@@ -5,3 +5,15 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App: Application()
+
+//Redo the swipe refresh so it does not have a fixed time but
+//disappears when it should depending on the state
+
+//Handle Error states with connection issues
+//Connection - show a dialog with a try again button
+
+//Cancel the data fetch when user goes to detail screen while fetching data
+//For now, show a dialog witk OK and Retry buttons. Put some picture in the background
+//if the user decides he does not want to fetch again
+
+//Add appbar with dark mode toggle and potentially search window
